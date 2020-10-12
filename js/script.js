618eaa17ce12bb3717d77ae86497cfaac4cce14c
Cyprian Panicz <rupertgupica@gmail.com>
2020-10-12 16:51:33 +0200

    Display article list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function titleClickHandler(event) {
-  console.log(event);
-
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('clickedElement (with plus): ' + clickedElement);
   /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -11,6 +12,8 @@ function titleClickHandler(event) {
 
   /* add class 'active' to the clicked link */
 
+  clickedElement.classList.add('active');
+
   /* remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('article.active');
@@ -20,10 +23,34 @@ function titleClickHandler(event) {
   }
   /* get 'href' attribute from the clicked link */
 
+  const href = clickedElement.getAttribute('href');
+  console.log(href);
+
   /* find the correct article using the selector (value of 'href' attribute) */
+  const article = document.querySelector(`article${href}`);
+  console.log(article);
+
 
   /* add class 'active' to the correct article */
+
+  article.classList.add('active');
 }
+
+function generateTitleLinks() {
+  const list = document.querySelector('.list');
+  list.innerHTML = "";
+  const articles = document.querySelectorAll('article.post');
+  let html = '';
+  for (let article of articles) {
+    const id = article.getAttribute("id");
+    const title = article.querySelector('.post-title').textContent;
+    const linkHTML = `<li><a href="#${id}"><span>${title}</span></a></li>`;
+    html += linkHTML;
+  }
+  list.innerHTML = html;
+}
+
+generateTitleLinks();
 
 const links = document.querySelectorAll('.titles a');
 
